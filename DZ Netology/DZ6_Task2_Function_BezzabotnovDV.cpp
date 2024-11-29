959a7b6b420e0753b21e71c115cfcdd18d4c1e00
Dmitry240281 <dmitry149@gmail.com>
2024-11-29 16:55:40 +0300

    Add files via upload

---
+++ DZ Netology/DZ6_Task2_Function_BezzabotnovDV.cpp
@@ -0,0 +1,23 @@
+﻿// Занятие 6, Функции. Задание 2
+
+#include <iostream>
+
+void degree(int volue, int power)
+{
+    setlocale(LC_ALL, "rus");
+    int result = 1;
+    for (int i = 0; i < power; i++)
+    {
+        result *= volue;
+    }
+    std::cout << volue << " в степени " << power << " = " << result << std::endl;
+}
+
+int main(int argc, char** argv)
+{
+    degree(5, 2);
+    degree(3, 3);
+    degree(4, 4);
+
+    return 0;
+}
---
+++ DZ Netology/DZ6_Task3_Function_BezzabotnovDV.cpp
@@ -0,0 +1,38 @@
+﻿// Занятие 6, Функции. Задание 3 Числа Фибоначчи
+/*Числа Фибоначчи — элементы числовой последовательности 0, 1,
+1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233..., в которой первые два числа равны 0 и 1,
+а каждое последующее число равно сумме двух предыдущих чисел.*/
+
+#include <iostream>
+
+int Fibonacci(int a)
+{
+    if (a == 0)
+    {
+        return 0;
+    }
+    else if (a == 1)
+    {
+        return 1;
+
+    }
+    else {
+        return a = Fibonacci(a - 1) + Fibonacci(a - 2);
+    }
+}
+
+int main(int argc, char** argv)
+{
+    setlocale(LC_ALL, "rus");
+    int a = 0;
+    std::cout << "Введите число: ";
+    std::cin >> a;
+    std::cout << "Числа Фибоначчи: ";
+    for (int i = 0; i < a; i++)
+    {
+        std::cout << Fibonacci(i) << ' ';
+    }
+    std::cout << std::endl;
+
+    return 0;
+}
