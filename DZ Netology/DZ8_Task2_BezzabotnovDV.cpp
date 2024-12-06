55af78a1f710999bc471e9433cb947706a369327
Dmitry240281 <dmitry149@gmail.com>
2024-12-06 14:09:52 +0300

    Add files via upload

---
+++ DZ Netology/DZ8_Task2_BezzabotnovDV.cpp
@@ -0,0 +1,39 @@
+﻿// Задача 2. Одинаковые, но разные функции
+
+#include <iostream>
+
+namespace mat_sum {
+    int calc(int x, int y) {
+        return x + y;
+    }
+}
+
+namespace mat_sub {
+    int calc(int x, int y) {
+        return x - y;
+    }
+}
+
+namespace mat_mul {
+    int calc(int x, int y) {
+        return x * y;
+    }
+}
+
+namespace mat_div {
+    int calc(int x, int y) {
+        return x / y;
+    }
+}
+
+int main()
+{
+    setlocale(LC_ALL, "rus");
+    int x{ 6 };
+    int y{ 9 };
+    std::cout << "x = " << x << ", y = " << y << std::endl;
+    std::cout << "Сложение: " << mat_sum::calc(x, y) << std::endl;
+    std::cout << "Вычитание: " << mat_sub::calc(x, y) << std::endl;
+    std::cout << "Умножение: " << mat_mul::calc(x, y) << std::endl;
+    std::cout << "Деление: " << mat_div::calc(x, y) << std::endl;
+}
