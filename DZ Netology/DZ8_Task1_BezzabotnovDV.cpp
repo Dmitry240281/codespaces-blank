b67d34f0a87ee6e411ef3f8eda29c7d50b21088a
Dmitry240281 <dmitry149@gmail.com>
2024-12-06 14:08:36 +0300

    Add files via upload

---
+++ DZ Netology/DZ8_Task1_BezzabotnovDV.cpp
@@ -0,0 +1,18 @@
+﻿// Задача 1. Считающая функция
+
+#include <iostream>
+static int i = 1;
+void counting_function() {
+    std::cout << "Количество вызовов функции counting_function(): " << i << std::endl;
+    i++;
+}
+
+int main(int argc, char** argv)
+{
+    setlocale(LC_ALL, "rus");
+    for (int i = 0; i < 15; i++)
+    {
+        counting_function();
+    }
+}
+
