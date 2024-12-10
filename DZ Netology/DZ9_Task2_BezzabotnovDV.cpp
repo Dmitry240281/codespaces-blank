448add1ed877716268a6c6e4d1ed8cdff5b577dc
Dmitry240281 <dmitry149@gmail.com>
2024-12-10 15:17:25 +0300

    Add files via upload

---
+++ DZ Netology/DZ9_Task2_BezzabotnovDV.cpp
@@ -0,0 +1,19 @@
+﻿// Задача 2. Снова swap
+
+#include <iostream>
+
+void swap(int &x, int &y) {
+    ((short*)&x)[1] = ((short*)&y)[0];
+    *((short*)&y) = ((short*)&x)[0];
+    *((short*)&x) = ((short*)&x)[1];
+    ((short*)&x)[1] = 0;
+}
+
+int main()
+{
+    int a = 5, b = 8;
+    std::cout << "a = " << a << ", b = " << b << std::endl;
+    swap(a, b);
+    std::cout << "a = " << a << ", b = " << b << std::endl;
+}
+
