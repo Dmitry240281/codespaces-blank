18c8d523842b4670295264884bbf116a24db0600
Dmitry240281 <dmitry149@gmail.com>
2024-12-10 14:40:38 +0300

    Add files via upload

---
+++ DZ Netology/DZ9_Task1_BezzabotnovDV.cpp
@@ -0,0 +1,22 @@
+﻿// Задача 1. Вывод массива
+
+#include <iostream>
+
+void print(int* arr, int size) {
+    int i;
+    for (i = 0; i < size; i++) std::cout << arr[i] << ' ';
+    std::cout << std::endl;
+}
+
+int main()
+{
+    int ar1[] = { 57, 25, 4 };
+    int ar2[] = { 125, 16, 5, 19, 51 };
+    int ar3[] = { 1, 5, 9, 3, 8, 32 };
+    print(ar1, 3);
+    print(ar2, 5);
+    print(ar3, 6);
+
+    return 0;
+}
+
