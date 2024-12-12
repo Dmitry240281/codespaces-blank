4e15d8b0401019e5f11815748088039ff526ae87
Dmitry240281 <dmitry149@gmail.com>
2024-12-12 21:15:05 +0300

    Add files via upload

---
+++ DZ Netology/DZ10_Task1_BezzabotnovDV.cpp
@@ -0,0 +1,28 @@
+﻿// Задача 1. Ввод массива с консоли
+
+#include <iostream>
+
+int main()
+{
+    setlocale(LC_ALL, "rus");
+    int size = 0;
+    std::cout << "Пожалуйста, введите размер массива: ";
+    std::cin >> size;
+    std::cout << std::endl;
+    int* arr = new int[size]();
+    std::cout << "Введите значения массива: ";
+    std::cout << std::endl;
+    for (int i = 0; i < size; ++i) {
+        std::cout << "arr[" << i << "] = "; 
+        std::cin >> arr[i];
+    }
+    std::cout << std::endl;
+    std::cout << "Введённый массив: ";
+    for (int i = 0; i < size; ++i) {
+        std::cout << arr[i] << ' ';
+    }
+    delete[] arr;
+
+    return 0;
+}
+
