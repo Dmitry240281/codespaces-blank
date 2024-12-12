d69a8e6c523ab7eb0a05e34eedf96a1e6bea0dcc
Dmitry240281 <dmitry149@gmail.com>
2024-12-12 21:40:27 +0300

    Add files via upload

---
+++ DZ Netology/DZ10_Task2_BezzabotnovDV.cpp
@@ -0,0 +1,26 @@
+﻿// Задача 2. Создатель массивов
+
+#include <iostream>
+
+double* create_array(int size) {
+    double* arr = new double[size]();
+    return arr;
+}
+
+int main()
+{
+    setlocale(LC_ALL, "rus");
+    int size = 0;
+    std::cout << "Введите размер массива: ";
+    std::cin >> size;
+    double* ext_arr = create_array(size);
+    std::cout << "Массив: ";
+    for (int i = 0; i < size; ++i) {
+        std::cout << ext_arr[i] << ' ';
+    }
+    
+    delete[] ext_arr;
+
+    return 0;
+}
+
