06472b9c384d3d7cc3b3d0e43141c1d8bbe6538f
Dmitry240281 <dmitry149@gmail.com>
2024-12-17 11:52:47 +0300

    Add files via upload

---
+++ DZ Netology/DZ11_Task1_BezzabotnovDV.cpp
@@ -0,0 +1,27 @@
+﻿// Задача 1. Приветствие
+
+#include <iostream>
+#include <Windows.h>
+#include <string>
+
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    std::string name;
+    std::string fam;
+    std::string otch;
+    std::string FIO;
+    std::cout << "Давайте познакомимся" << std::endl;
+    std::cout << "Введите ваше фамилию: ";
+    std::cin >> fam;
+    std::cout << "Введите ваше имя: ";
+    std::cin >> name;
+    std::cout << "Введите ваше отчество: ";
+    std::cin >> otch;
+    FIO = "Здравствуйте, " + fam + " " + name + " " + otch + "!";
+    std::cout << FIO << std::endl;
+
+    return 0;
+}
+
