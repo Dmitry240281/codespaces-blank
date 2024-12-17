46c553606c2d36832c5c3b674b14d0ac40ffc5f5
Dmitry240281 <dmitry149@gmail.com>
2024-12-17 12:32:42 +0300

    Add files via upload

---
+++ DZ Netology/DZ11_Task2_BezzabotnovDV.cpp
@@ -0,0 +1,29 @@
+﻿// Задача 2. Угадайка
+
+#include <iostream>
+#include <Windows.h>
+#include <string>
+
+std::string _compare(std::string sec) {
+    std::string word;
+    do {
+        std::cout << "Угадай ягоду: ";
+        std::cin >> word;
+        if (word.compare(sec) != 0) std::cout << "Неправильно" << std::endl;
+    } while (word.compare(sec) != 0);
+    std::string otv = "Правильно, вы угадали: " + word;
+    return otv;
+}
+
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251); 
+    std::string secret{ "малина" };
+    std::cout << "Давай поиграем" << std::endl;
+
+    std::cout << _compare(secret) << std::endl;
+
+    return 0;
+}
+
