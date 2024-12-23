30bb480b774b6394ffb867277cd0e7d6c7a54f9d
Dmitry240281 <dmitry149@gmail.com>
2024-12-23 11:15:58 +0300

    Add files via upload

---
+++ DZ Netology/DZ12_Task1_BezzabotnovDV.cpp
@@ -0,0 +1,25 @@
+﻿//Задача 1. Вывод файла
+
+#include <iostream>
+#include <fstream>
+
+int main()
+{
+    setlocale(LC_ALL, "rus");
+    std::string buff;
+    std::ifstream fin("in.txt");
+
+    if (!fin.is_open()) {
+        std::cout << "Файл не нейден. Проверьте наличие и название файла" << std::endl;
+        return 1;
+    }
+
+    while (fin >> buff) {
+        std::cout << buff << std::endl;
+        if (fin.eof()) break;
+    }
+    fin.close();
+
+    return EXIT_SUCCESS;
+}
+
