11fc22165c70300d5973bca4379b03c98114b11c
Dmitry240281 <dmitry149@gmail.com>
2025-04-04 09:39:24 +0300

    First

---
+++ DZ Netology/algocpp_DZ2_Task1_BezzabotnovDV.cpp
@@ -0,0 +1,34 @@
+﻿// Числа Фибоначчи через рекурсивный подсчёт. Сложность O(2^n), память О(1)
+
+#include <iostream>
+
+int Fibonacci(int a)
+{
+    if (a == 0)
+    {
+        return 0;
+    }
+    else if (a == 1)
+    {
+        return 1;
+    }
+    else {
+        return a = Fibonacci(a - 1) + Fibonacci(a - 2);
+    }
+}
+
+int main(int argc, char** argv)
+{
+    setlocale(LC_ALL, "rus");
+    int a = 0;
+    std::cout << "Введите число: ";
+    std::cin >> a;
+    std::cout << "Числа Фибоначчи: ";
+    for (int i = 0; i < a; i++)
+    {
+        std::cout << Fibonacci(i) << ' ';
+    }
+    std::cout << std::endl;
+
+    return EXIT_SUCCESS;
+}
---
+++ DZ Netology/algocpp_DZ2_Task2_BezzabotnovDV.cpp
@@ -0,0 +1,44 @@
+﻿/// Числа Фибоначчи c использованием массива. Массив ускоряет работу. Алгосложность составляет O(n) и память O(n);
+
+#include <iostream>
+
+double* Fibonacci(double* arr, int size)
+{
+    if (size == 0)
+    {
+        arr[0] = 0;
+        return arr;
+    }
+    else if (size == 1)
+    {
+        arr[0] = 0;
+        arr[1] = 1;
+        return arr;
+    }
+    else {
+        arr[0] = 0;
+        arr[1] = 1;
+        for (int i = 2; i < size; i++)
+            arr[i] = arr[i - 1] + arr[i - 2];
+        return arr;
+    }
+}
+
+int main(int argc, char** argv)
+{
+    setlocale(LC_ALL, "rus");
+    
+    int a = 0;
+    std::cout << "Введите число: ";
+    std::cin >> a;
+    double* array = new double[a];
+    Fibonacci(array, a);
+    std::cout << "Числа Фибоначчи: ";
+    for (int i = 0; i < a; i++)
+    {
+        std::cout << array[i] << ' ';
+    }
+    std::cout << std::endl;
+
+    return EXIT_SUCCESS;
+}
