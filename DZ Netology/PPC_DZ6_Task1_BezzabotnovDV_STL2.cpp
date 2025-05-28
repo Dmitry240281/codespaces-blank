233755a2419bb530e6d0b876b28530a26b6aa577
Dmitry240281 <dmitry149@gmail.com>
2025-05-28 09:14:31 +0300

    DZ6 STL2

---
+++ DZ Netology/PPC_DZ6_Task1_BezzabotnovDV_STL2.cpp
@@ -0,0 +1,26 @@
+﻿// Задача 1. Уникальный вектор
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+int main()
+{
+    std::vector<int> vec = { 1, 1, 2, 5, 6, 1, 2, 4 };
+    std::cout << "[IN]: ";
+    for (auto i : vec)
+        std::cout << i << " ";
+    std::cout << std::endl;
+
+    std::sort(vec.begin(), vec.end()); // сначала сортируем vector
+
+    auto it = std::unique(vec.begin(), vec.end());
+    vec.erase(it, vec.end()); // удаляем одинаковые значения
+    std::cout << "[OUT]: ";
+    for (auto i : vec)
+        std::cout << i << " ";
+    std::cout << std::endl;
+
+    return 0;
+}
+
---
+++ DZ Netology/PPC_DZ6_Task2_BezzabotnovDV_STL2.cpp
@@ -0,0 +1,54 @@
+﻿// Задача 2. Печать контейнера
+
+#include <iostream>
+#include <set>
+#include <list>
+#include <vector>
+#include <map>
+
+template <typename T>
+void print_container(std::set<T> set) {
+    std::cout << "Set container: ";
+    for (auto s : set)
+        std::cout << s << " ";
+    std::cout << std::endl;
+}
+template <typename T>
+void print_container(std::list<T> list) {
+    std::cout << "List container: ";
+    for (auto l : list)
+        std::cout << l << " ";
+    std::cout << std::endl;
+}
+template <typename T>
+void print_container(std::vector<T> vec) {
+    std::cout << "Vector container: ";
+    for (auto v : vec)
+        std::cout << v << " ";
+    std::cout << std::endl;
+}
+template <typename A, typename B>
+void print_container(std::map<A, B> map) {
+    std::cout << "Map container: " << std::endl;
+    for (auto m : map)
+        std::cout <<"[ " << m.first << ", " << m.second << " ]" << std::endl;
+    std::cout << std::endl;
+}
+
+int main()
+{
+    std::set<std::string> test_set = { "one", "two", "three", "four" };
+    print_container(test_set); // four one three two. Set - сортирует по возрастанию
+
+    std::list<std::string> test_list = { "one", "two", "three", "four" };
+    print_container(test_list); // one, two, three, four
+
+    std::vector<std::string> test_vector = { "one", "two", "three", "four" };
+    print_container(test_vector); // one, two, three, four
+
+    std::map<int, std::string> test_map = { {1,"one"}, {2,"two"},{3,"three"},{4,"four"} };
+    print_container(test_map);
+
+    return 0;
+}
+
