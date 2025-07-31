139600976aaf3c34e492530b089d5f380032868e
Dmitry240281 <dmitry149@gmail.com>
2025-07-31 13:57:36 +0300

    MT DZ1

---
+++ DZ Netology/MT_DZ1_Task1_BezzabotnovDV.cpp
@@ -0,0 +1,51 @@
+﻿//Задание 1
+//чередь клиентов
+
+#include <iostream>
+#include <thread>
+#include <chrono>
+using namespace std;
+
+void first_window(int start)
+{
+    int end = start;
+    cout << "Первое окно: начало работать!\n";
+    for (int i = 0; i < end; ++i)
+    {
+        ++start;
+        this_thread::sleep_for(2s);
+    }
+    cout << "Первое окно: закончило работать!" << endl;
+}
+void second_window(int start)
+{
+    cout << "Второе окно: начало работать!\n";
+    do
+    {
+        --start;
+        this_thread::sleep_for(3s);
+    } while (start > 0);
+    cout << "Второе окно: закончило работать!" << endl;
+}
+
+int main()
+{
+    setlocale(LC_ALL, "rus");
+    int queue = 10;
+    auto start = chrono::system_clock::now();
+    time_t now_t = chrono::system_clock::to_time_t(start);
+    cout << "Время начала работы: ";
+    cout << ((now_t / 3600 + 3) % 24) << " ч. " << (now_t / 60 % 60) << " м. " << (now_t % 6) << " c." << endl;
+    thread window1(first_window, queue);
+    thread window2(second_window, queue);
+    window1.join();
+    auto end1 = chrono::system_clock::now();
+    chrono::duration<double> elapsed_seconds1 = end1 - start;
+    cout << "Время работы 1 окна: " << elapsed_seconds1.count() << "s" << endl;
+    window2.join();
+    auto end2 = chrono::system_clock::now();
+    chrono::duration<double> elapsed_seconds2 = end2 - start;
+    cout << "Время работы 2 окна: " << elapsed_seconds2.count() << "s" << endl;
+
+    return 0;
+}
---
+++ DZ Netology/MT_DZ1_Task2_BezzabotnovDV.cpp
@@ -0,0 +1,85 @@
+﻿// Задание 2
+// Параллельные вычисления
+
+#include <iostream>
+#include <vector>
+#include <thread>
+#include <chrono>
+
+using namespace std;
+
+void SumTwoVectors(double& sumvec, vector<int> vec1, vector<int> vec2) {
+    
+    for (int i = 0; i < vec1.size(); ++i)
+    {
+        sumvec += vec1.at(i) + vec2.at(i);
+    }
+
+}
+
+int main()
+{
+    setlocale(LC_ALL, "rus");
+    cout << "Количество аппаратных ядер - " << std::thread::hardware_concurrency() << "\n\n";
+    cout << "\t\t1'000" << "\t\t10'000" << "\t\t100'000" << "\t\t1'000'000" << endl;
+    
+    vector<int> vector1;
+    vector<int> vector2;
+    
+    vector<int> num_threads{ 1, 2, 4, 8, 16 }; // Заданное киличество потоков
+    
+    vector<int> size_vector{ 1'000, 10'000, 100'000, 1'000'000 }; // размер векторов
+
+    for (auto& count_thr : num_threads)
+    {
+        cout << endl << count_thr << " ядер\t\t";
+
+        for (auto& count_vec : size_vector)
+        {
+            vector1.resize(count_vec, 1);
+            vector2.resize(count_vec, 1);
+            vector<std::thread> threads;
+            double sum_vectors = 0;
+
+            int part_size = count_vec / count_thr;
+
+            auto start = chrono::high_resolution_clock::now();
+            for (int i = 0; i < count_thr; i++)
+            {
+                vector<int> new_vector1;
+                vector<int> new_vector2;
+                int right_border = 0;
+                
+                if (i != count_thr - 1)
+                {
+                    right_border = part_size * (i + 1);
+                }
+                else
+                {
+                    right_border = count_vec;
+                }
+                
+                right_border = count_vec;
+                for (int j = part_size * i; j < right_border; ++j)
+                {
+                    new_vector1.push_back(vector1.at(j));
+                    new_vector2.push_back(vector2.at(j));
+                }
+
+                threads.push_back(thread(SumTwoVectors, std::ref(sum_vectors), new_vector1, new_vector2));
+            }
+            for (auto& it : threads)
+            {
+                it.join();
+            }
+
+            auto end = std::chrono::high_resolution_clock::now();
+            chrono::duration<double> elapsed_seconds = end - start;
+            cout << elapsed_seconds.count() << " мс\t";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
+
