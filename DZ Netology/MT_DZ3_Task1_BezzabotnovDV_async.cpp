e202e7142a249457fa9c109bca41c9bbeeae3a71
Dmitry240281 <dmitry149@gmail.com>
2025-08-07 16:55:26 +0300

    async

---
+++ DZ Netology/MT_DZ3_Task1_BezzabotnovDV_async.cpp
@@ -0,0 +1,48 @@
+﻿// MT_DZ3_Task1_BezzabotnovDV_async.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <chrono>
+#include <future>
+#include <thread>
+#include <vector>
+
+using namespace std;
+
+promise<vector<int>::iterator> find_min_value(vector<int>& v, vector<int>::iterator it) {
+    promise<vector<int>::iterator> promise;
+    auto min_it = it;
+    for (auto it2 = it + 1; it2 < v.end(); ++it2)
+    {
+        if ((*it2) < (*min_it)) {
+            min_it = it2;
+        }
+    }
+    promise.set_value(min_it);
+    return promise;
+}
+
+int main()
+{
+    vector<int> V{ 1,35,64,23,3,78,5,89,34,56,178,27,95,4,95,3 };
+    
+    auto start = chrono::system_clock::now();
+    for (auto it = V.begin(); it < V.end() - 1; ++it)
+    {
+        std::promise<vector<int>::iterator> Promise = find_min_value(V, it);
+        std::future<vector<int>::iterator> Future = Promise.get_future();
+        
+        auto minit = Future.get();
+        swap(*it, *minit);
+    }
+    auto end = chrono::system_clock::now();
+    chrono::duration<double, milli> time = end - start;
+    cout << "Time = " << time.count() << " ms" << endl;
+
+    cout << "Vector: " << endl;
+    for (auto i : V) cout << i << ' ';
+    cout << endl;
+
+    return 0;
+}
+
---
+++ DZ Netology/MT_DZ3_Task2_BezzabotnovDV_async.cpp
@@ -0,0 +1,57 @@
+﻿// Задание 2
+// parallel_for_each
+
+#include <iostream>
+#include <chrono>
+#include <future>
+#include <vector>
+
+using namespace std;
+
+template<typename Iterator, typename T>
+void parallel_for_each(Iterator first, Iterator last, T&& func, size_t min_size = 1000)
+{
+    const size_t size = std::distance(first, last);
+
+    if (size < min_size)
+    {
+        std::for_each(first, last, forward<T>(func));
+        return;
+    }
+    else {
+        Iterator middle = first;
+        std::advance(middle, size / 2);
+
+        auto future = std::async(launch::async,
+            [first, middle, &func, min_size]()
+            {
+                parallel_for_each(first, middle, forward<T>(func), min_size);
+            });
+
+        parallel_for_each(middle, last, forward<T>(func), min_size);
+        future.get();
+    }
+}
+
+int main()
+{
+    vector<int> V(1'000'000, 2);
+
+    auto process_func = [](int& n) { n += 2; }; // Обрабатывающая функция
+
+    auto start = chrono::system_clock::now();
+    for_each(V.begin(), V.end(), process_func);
+    auto end = chrono::system_clock::now();
+    chrono::duration<double, milli> time = end - start;
+    cout << "Time for_each = " << time.count() << " ms" << endl;
+
+    auto start1 = chrono::system_clock::now();
+    parallel_for_each(V.begin(), V.end(), process_func);
+    cout << endl;
+    auto end1 = chrono::system_clock::now();
+    chrono::duration<double, milli> time1 = end1 - start1;
+    cout << "Time parallel_for_each = " << time1.count() << " ms" << endl;
+
+    return 0;
+}
+
